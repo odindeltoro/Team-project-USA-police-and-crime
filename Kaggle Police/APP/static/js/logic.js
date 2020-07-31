1d5e2b8a9688b530792e4e06760d8770a322dfea
Martin Jr <martinjaramillo35@gmail.com>
2020-07-30 21:10:11 -0500

    FinalVersionAbrahamBernardoMartin

--- Kaggle Police/APP/static/js/logic.js
+++ Kaggle Police/APP/static/js/logic.js
@@ -310,9 +310,123 @@ function map2() {
     });
 }
 
+function bernardo() {
+    d3.csv(`/search4/`).then(Statedata => {
+        console.log("Este", Statedata);
+
+        let States = Statedata.map(d => d.State);
+        console.log("States", States);
+
+        let Police = Statedata.map(d => d.Police_Employment);
+        console.log("Police", Police);
+
+        new Chart(document.getElementById("polar-chart2"), {
+            type: "polarArea",
+            data: {
+                labels: Statedata.map(d => d.State),
+                datasets: [
+                    {
+                        label: "Police count",
+                        backgroundColor: ["#00bcd4", "#b2ebf2", "#ff5722", "#dd2c00", "#411f1f", "#411f1f", "#86c4ba", "#4b5d67",
+                            "#322f3d", "#59405c", "#87556f", "#4f8a8b", "#fbd46d", "#f09ae9", "#ffc1fa", "#ffe78f",
+                            "#ffd36b", "#62760c", "#535204", "#523906", "#848ccf", "#93b5e1", "#ffe4e4", "#be5683",
+                            "#206a5d", "#1f4068", "#e3dfc8", "#f5f1da", "#96bb7c", "#eebb4d", "#6f4a8e", "#221f3b",
+                            "#ffdbc5", "#cf1b1b", "#900d0d", "#423144", "#006a71", "#ffffdd", "#cbeaed", "#d3de32",
+                            "#394989", "#4ea0ae", "#fff48f", "#1b262c", "#0f4c75", "#3282b8", "#bbe1fa", "#ffe0f7",
+                            "#fe91ca", "#ede682", "#e84a5f", "#f6ab6c"],
+                        data: Statedata.map(d => d.Police_Employment)
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: false,
+                    text: "Police Employment between 1985-2018",
+                    fontSize: 22,
+                    position: 'left'
+                },
+                legend: {
+                    position: 'right'
+                }
+            },
+            animation: {
+                animateRotate: true,
+                animateScale: true
+            }
+        });
+
+
+    });
+}
+
+function bernardo1() {
+    d3.csv(`/search5/`).then(Arrestdata => {
+        console.log("ESTE!", Arrestdata);
+
+        let States = Arrestdata.map(d => d.State);
+        console.log("States", States);
+
+        let Arrests = Arrestdata.map(d => d.Arrests);
+        console.log("Arrests", Arrests);
+        d3.csv(`/search6/`).then(Crimedata => {
+            console.log("ESTE2!!", Crimedata)
+            let Crimes = Crimedata.map(d => d.Crimes);
+            console.log("Crimes", Crimes);
+
+            new Chart(document.getElementById("radar-chart"), {
+            type: 'radar',
+            data: {
+                labels: States,
+                datasets: [
+                {
+                    label: "Arrests",
+                    fill: true,
+                    backgroundColor: "#beebe9",
+                    borderColor: "#32afa9",
+                    pointBorderColor: "#f17e7e",
+                    pointBackgroundColor: "#fdd998",
+                    data: Arrests
+                }, {
+                    label: "Crimes",
+                    fill: true,
+                    backgroundColor: "#daf1f9",
+                    borderColor: "#588da8",
+                    pointBorderColor: "#e4508f",
+                    pointBackgroundColor: "#df7599",
+                    data: Crimes
+                }
+                ]
+            },
+            options: {
+                responsive: true,
+                title: {
+                display: false,
+                text: "Arrests and Crimes between 1985-2018",
+                fontSize: 22,
+                position: 'right'
+                },
+                legend: {
+                position: 'top'
+                }
+            },
+            animation: {
+                animateRotate: true,
+                animateScale: true
+            }
+            });
+
+
+
+        });//Closing second dataread
+    });//Closing first dataread
+}//Closing function
+
 goGetResults();
 graph();
 graph2();
 map2();
+bernardo();
+bernardo1();
 d3.select("#buscarTipo").on("change", deletemap);
 d3.select("#state").on("change", deletegraph);
---
+++ Kaggle Police/APP/static/js/polar.js
@@ -0,0 +1,46 @@
+// Request data using D3
+d3.csv("../static/data/statePoliceEmployment.csv").then(function (Statedata) {
+  console.log(Statedata);
+
+  let States = Statedata.map(d => d.State);
+  console.log("States", States);
+
+  let Police = Statedata.map(d => d.Police_Employment);
+  console.log("Police", Police);
+
+  new Chart(document.getElementById("polar-chart2"), {
+    type: "polarArea",
+    data: {
+      labels: Statedata.map(d => d.State),
+      datasets: [
+        {
+          label: "Police count",
+          backgroundColor: ["#00bcd4", "#b2ebf2", "#ff5722", "#dd2c00", "#411f1f", "#411f1f", "#86c4ba", "#4b5d67",
+            "#322f3d", "#59405c", "#87556f", "#4f8a8b", "#fbd46d", "#f09ae9", "#ffc1fa", "#ffe78f",
+            "#ffd36b", "#62760c", "#535204", "#523906", "#848ccf", "#93b5e1", "#ffe4e4", "#be5683",
+            "#206a5d", "#1f4068", "#e3dfc8", "#f5f1da", "#96bb7c", "#eebb4d", "#6f4a8e", "#221f3b",
+            "#ffdbc5", "#cf1b1b", "#900d0d", "#423144", "#006a71", "#ffffdd", "#cbeaed", "#d3de32",
+            "#394989", "#4ea0ae", "#fff48f", "#1b262c", "#0f4c75", "#3282b8", "#bbe1fa", "#ffe0f7",
+            "#fe91ca", "#ede682", "#e84a5f", "#f6ab6c"],
+          data: Statedata.map(d => d.Police_Employment)
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: "Police Employment between 1985-2018",
+        fontSize: 22,
+        position: 'left'
+      },
+      legend: {
+        position: 'right'
+      }
+    },
+    animation: {
+      animateRotate: true,
+      animateScale: true
+    }
+  });
+});
